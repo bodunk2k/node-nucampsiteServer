0f189475ba4a7cf8724205cd84391e88d0d95b31
Brent Bowen <bodunk2k@aol.com>
2020-06-17 18:19:59 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = {
     'secretKey': '12345-67890-09875-54321',
-    'mongoUrl': 'mongodb+srv://admin:admin@cluster0-82mcz.gcp.mongodb.net/nucamp'
+    'mongoUrl': 'mongodb+srv://admin:admin@cluster0-82mcz.gcp.mongodb.net/nucamp',
+    'facebook': {
+        clientId: '704850703418742',
+        clientSecret: 'a326d20e35890b8ca64c06cb5d9fff55'
+    }
 }
 
 //'mongodb+srv://admin:admin@cluster0-82mcz.gcp.mongodb.net/nucamp'
